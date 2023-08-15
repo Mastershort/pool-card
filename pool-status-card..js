ffb47bad9a57cbdff01edb0a2647151abfeee75c
Mastershort <BeckerTorsten82@googlemail.com>
2023-08-15 14:49:26 +0200

    Intial5

--- pool-status-card..js
+++ pool-status-card..js
@@ -1,27 +1,31 @@
 class PoolStatusCard extends HTMLElement {
-    setConfig(config) {
-      if (!config.entity) {
-        throw new Error('You need to define an entity');
-      }
-      this.config = config;
+  setConfig(config) {
+    if (!config.orp_entity || !config.ph_entity || !config.temperature_entity) {
+      throw new Error('You need to define ORP, pH, and temperature entities');
     }
-  
-    set hass(hass) {
-      const entity = hass.states[this.config.entity];
-      const card = document.createElement('ha-card');
-      card.header = 'Lamp Status';
-      card.innerHTML = `
-        <div class="card-content">
-          The lamp is ${entity.state}
-        </div>
-      `;
-      this.appendChild(card);
-    }
-  
-    getCardSize() {
-      return 1;
-    }
+    this.config = config;
   }
-  
-  customElements.define('pool-status-card', PoolStatusCard);
-  
+
+  set hass(hass) {
+    const orpValue = hass.states[this.config.orp_entity].state;
+    const phValue = hass.states[this.config.ph_entity].state;
+    const temperatureValue = hass.states[this.config.temperature_entity].state;
+
+    const card = document.createElement('ha-card');
+    card.header = 'Pool Status';
+    card.innerHTML = `
+      <div class="card-content">
+        <p>ORP: ${orpValue}</p>
+        <p>pH: ${phValue}</p>
+        <p>Temperature: ${temperatureValue}</p>
+      </div>
+    `;
+    this.appendChild(card);
+  }
+
+  getCardSize() {
+    return 3; // Adjust as needed
+  }
+}
+
+customElements.define('pool-status-card', PoolStatusCard);
